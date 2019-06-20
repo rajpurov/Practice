2d8c27afe49dd15afecf5e5b95786c450bd26f7f
rajpurov <rajpurov@sheridancollege.ca>
2019-06-20 11:29:15 -0400

    ThursdayExample

---
+++ ThursdayExample/src/thursdayexample/Professor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thursdayexample;
+
+/**
+ *
+ * @author owner
+ */
+public class Professor 
+{
+    private String name;
+    private int Salary;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the Salary
+     */
+    public int getSalary() {
+        return Salary;
+    }
+
+    /**
+     * @param Salary the Salary to set
+     */
+    public void setSalary(int Salary) {
+        this.Salary = Salary;
+    }
+    
+}
---
+++ ThursdayExample/src/thursdayexample/Student.java
@@ -0,0 +1,32 @@
+package thursdayexample;
+
+public class Student {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * 
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
